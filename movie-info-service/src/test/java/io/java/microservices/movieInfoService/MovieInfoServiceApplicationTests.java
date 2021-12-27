662e6130cd2c47574db9d5ae5dd0a892e160c3d0
codeChef8888 <eryashre7777@gmail.com>
2021-12-27 14:40:33 +0545

    microservicesBasics

---
+++ movie-info-service/src/test/java/io/java/microservices/movieInfoService/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.microservices.movieInfoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ratings-data-service/src/test/java/io/java/microservices/ratingsDataService/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.microservices.ratingsDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
